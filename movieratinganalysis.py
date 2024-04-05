d400846a577cc70d80ebea138dbd05b8d60befd8
sandhiyachamundeeswari <166101572+sandhiyachamundeeswari@users.noreply.github.com>
2024-04-05 15:08:20 +0530

    Add files via upload

---
+++ movieratinganalysis.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+movies = pd.read_csv("movies.dat", delimiter='::')
+print(movies.head())
+movies.columns = ["ID", "Title", "Genre"]
+print(movies.head())
+ratings = pd.read_csv("ratings.dat", delimiter='::')
+print(ratings.head())
+ratings.columns = ["User", "ID", "Ratings", "Timestamp"]
+print(ratings.head())   
+data = pd.merge(movies, ratings, on=["ID", "ID"])
+print(data.head())
+ratings = data["Ratings"].value_counts()
+numbers = ratings.index
+quantity = ratings.values
+import plotly.express as px
+fig = px.pie(data, values=quantity, names=numbers)
+fig.show()
+data2 = data.query("Ratings == 10")
+print(data2["Title"].value_counts().head(10))
+
+#output is available
